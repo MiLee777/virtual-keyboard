7c74f7e2bcd646d2b36ac6ecb8866759ca41147e
MiLee777 <midyashka1506@gmail.com>
2023-04-29 20:31:31 +0300

    feat: add special characters

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ containerElements.appendChild(containerKeyboard);
 
 //console.log(containerElements);
 
-const keyboard = [96, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 45, 61, 'Backspace', 'Tab', 113, 119, 101, 114, 116, 121, 117, 105, 111, 112, 91, 93, 92, 'DEL', 'Caps Lock', 97, 115, 100, 102, 103, 104, 106, 107, 108, 59, 39, 'ENTER', 'Shift', 122, 120, 99, 118, 98, 110, 109, 44, 46, 47, '↑', 'Shift', 'Ctrl', 'Win', 'Alt', 32, 'Alt', 'Ctrl', '←', '↓', '→'];
+//const keyboard = [96, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 45, 61, 'Backspace', 'Tab', 113, 119, 101, 114, 116, 121, 117, 105, 111, 112, 91, 93, 92, 'DEL', 'Caps Lock', 97, 115, 100, 102, 103, 104, 106, 107, 108, 59, 39, 'ENTER', 'Shift', 122, 120, 99, 118, 98, 110, 109, 44, 46, 47, '↑', 'Shift', 'Ctrl', 'Win', 'Alt', 32, 'Alt', 'Ctrl', '←', '↓', '→'];
 
 /*document.onkeypress = function(event) {
     //console.log(event);
@@ -30,7 +30,7 @@ const keyboard = [96, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 45, 61, 'Backspace
     console.log(keyboard);
 }*/
 
-function init() {
+/*function init() {
     let out = '';
     for (let i = 0; i < keyboard.length; i++) {
         if (i==14 || i==29 || i==42 || i==56) {
@@ -41,4 +41,23 @@ function init() {
     document.querySelector('.keyboard').innerHTML = out;
 }
 
+init();*/
+
+
+const keyboard = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '+', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'DEL', 'Caps Lock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'ENTER', 'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '↑', 'Shift', 'Ctrl', 'Win', 'Alt', '', 'Alt', 'Ctrl', '←', '↓', '→'];
+function init() {
+    let out = '';
+    for (let i = 0; i < keyboard.length; i++) {
+        if (i==14 || i==29 || i==42 || i==55) {
+            out += '<div class="clearfix"></div>';
+        }
+        out += '<div class="keys" >' + String(keyboard[i]) + '</div>';
+    }
+    document.querySelector('.keyboard').innerHTML = out;
+}
+
 init();
+
+
+
+
